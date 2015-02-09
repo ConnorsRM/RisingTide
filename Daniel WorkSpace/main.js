5a07eba3e4f3bde30ecee146cde64b3317e0e21e
Daniel Luna <danieljluna0@gmail.com>
2015-02-08 20:34:21 -0800

    Tile Engine 1.0
    Can Handle Draw Requests as well as return the object in question.
    Considering adding functionality for a sprite sheet. Let me know what
    you guys think or if you have questions

---
+++ Daniel WorkSpace/main.js
@@ -0,0 +1,72 @@
+//-----------------------------------------------------
+// Game Creation and Update
+//-----------------------------------------------------
+
+//Initialize some global variables about the working space:
+var canvas;
+var context;
+var c_width;
+var c_height;
+
+//Create images
+var gameImages = 3;
+var loadedImages = 0;
+var water;
+var sand;
+var grass;
+
+//tile engine
+var tileEngine;
+
+function gameLoad() {
+    console.log("gameLoad();");
+    //Anything in here will be done only on load:
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+    c_width = canvas.width;
+    c_height = canvas.height;
+    
+    //Load Images
+    water = new Image();
+    water.src = "water.png";
+    water.onload = imageLoad;
+    sand = new Image();
+    sand.src = "sand.png";
+    sand.onload = imageLoad;
+    grass = new Image();
+    grass.src = "grass.png";
+    grass.onload = imageLoad;
+    
+    tileEngine = new TileEngine(10, 15, 40);
+    tileEngine.loadTile(water);
+    tileEngine.loadTile(sand);
+    tileEngine.loadTile(grass);
+};
+
+
+function imageLoad() {
+    ++loadedImages;
+    if (loadedImages == gameImages) {
+        initGame();
+    }
+};
+
+
+function initGame() {
+    for (var x = 0; x < 10; ++x) {
+        for (var y = 0; y < 20; ++y) {
+            tileEngine.getTile(x, y).tile = Math.floor(3*Math.random());
+        }
+    }
+    
+    //Draws Whole Map
+    tileEngine.draw({x: 0, y: 400});
+    
+    //Draws Cell (5, 5)
+    tileEngine.drawCell({x: 440, y: 20}, {x:5, y:5});
+    
+    //Draws Region
+    tileEngine.drawSection({x:0, y:0}, {x:20, y:50}, {x:300, y:400});
+    
+    console.log("Drawn at (10, 20)");
+};
---
+++ Daniel WorkSpace/tile.js
@@ -0,0 +1,152 @@
+//----------------------------------------
+//  Tile Engine Class
+//----------------------------------------
+
+//The TileEngine will handle the storage and drawing
+//  of a series of tiles denoting the world. Each cell
+//  will point to a Tile (a square image used to 
+//  represent this cell when drawing), as well as be
+//  able to discern if a point (x, y) is inside of it.
+
+
+var TileEngine = function(columns, rows, cellSize) {
+    //Assumes cellSize is the same as the Tile image
+    //  sizes to be loaded later. This may be updated
+    //  later if it's decided we need to figure out
+    //  how to scale.
+    
+    this.columns = columns;
+    this.rows = rows;
+    //Denotes the amount of columns and rows in the
+    //  tileMap.
+    
+    this.cellSize = cellSize;
+    //Denotes the expected size (width and height) of 
+    //  the image and thus the size of the tile drawn 
+    //  per cell.
+    
+    this.tileDictionary = [];
+    //Denotes the mapping of integers to Tiles (images)
+    //  for use by the cells in our TileEngine.
+    
+    this.tileCount = 0;
+    //Denotes the amount of Tiles loaded into the 
+    //  tileDictionary.
+    
+    this.tileMap = [];
+    //This double array holds all of the cells. The first
+    //  index denotes the x position of the cell returned,
+    //  and the second index denotes the y position of the
+    //  cell returned. Thus, the max number for the first
+    //  index = this.columns - 1, and the max number for
+    //  the second index = this.rows - 1.
+    for (var col = 0; col < this.columns; ++col) {
+        var tileColumn = [];
+        for (var row = 0; row < this.rows; ++row) {
+            var defaultCell = {
+                x: col,
+                y: row,
+                tile: -1,
+                elevation: 0
+            };
+            //Default cell definition defined here. (x, y)
+            //  simply denotes the position of this cell.
+            //  tile is set to null initially (null == -1),
+            //  and should be set to the desired tile via
+            //  the tileDictionary. elevation denotes the
+            //  height of this tile.
+            
+            tileColumn.push(defaultCell);
+        }
+        this.tileMap.push(tileColumn);
+    }
+    
+};
+
+
+TileEngine.prototype.loadTile = function(image){
+    //Adds an image to the tileDictionary for use by the 
+    //  TileEngine. Returns the integer the image was 
+    //  assigned, which will be the next unused integer.
+    //  Since image loading takes time, this should occur
+    //  before the tileImage is used.
+    //Assumes filename is a valid path to an image.
+    
+    this.tileDictionary[this.tileCount] = image;
+    ++this.tileCount;
+    return this.tileCount - 1;
+};
+
+
+TileEngine.prototype.getTile = function(x, y) {
+    //If x and y are within the bounds of the tileMap,
+    //  returns the cell at that position in the array.
+    if ((x >= 0) && (x < this.columns) &&
+        (y >= 0) && (y < this.rows))
+        return this.tileMap[x][y];
+    else
+        return false;
+};
+
+
+TileEngine.prototype.drawCell = function(pos, cell) {
+    //Draws the cell at (cell.x, cell.y) on the canvas at
+    //  (pos.x, pos.y).
+    var Tile = this.tileMap[cell.x][cell.y].tile;
+        if (Tile != -1)
+            context.drawImage(this.tileDictionary[Tile], pos.x, pos.y, this.cellSize, this.cellSize);
+};
+
+
+TileEngine.prototype.drawSection = function(pos, startDraw, endDraw) {
+    //Draws the cells that (even partially) fall within the
+    //  range of the rectangle denoted by the points startDraw
+    //  and endDraw. This range is based on the pixel 
+    //  representation of the tileMap. This region is then 
+    //  translated onto the context so that startDraw is at pos.
+    
+    var firstCell = {
+        x: Math.floor(startDraw.x / this.cellSize),
+        y: Math.floor(startDraw.y / this.cellSize)
+    };
+    
+    var offset = {
+        x: firstCell.x * this.cellSize - startDraw.x,
+        y: firstCell.y * this.cellSize - startDraw.y
+    };
+    
+    offset.x += pos.x;
+    offset.y += pos.y;
+    
+    var cellsWide = Math.ceil((endDraw.x - startDraw.x) / this.cellSize);
+    var cellsHigh = Math.ceil((endDraw.y - startDraw.y) / this.cellSize);
+    
+    for (var x = firstCell.x; x < cellsWide; ++x) {
+        for (var y = firstCell.y; y < cellsHigh; ++y) {
+            var drawAt = {
+                x: offset.x + (x - firstCell.x) * this.cellSize,
+                y: offset.y + (y - firstCell.y) * this.cellSize  
+            };
+            var Tile = this.tileMap[x][y].tile;
+            if (Tile != -1)
+                context.drawImage(this.tileDictionary[Tile], drawAt.x, drawAt.y, this.cellSize, this.cellSize);
+        }
+    }
+};
+
+
+TileEngine.prototype.draw = function(pos) {
+    //Draws the entire map with the cell (0, 0) at (pos.x, 
+    //  pos.y).
+    for (var x = 0; x < this.columns; ++x) {
+        for (var y = 0; y < this.rows; ++y) {
+            var offset = {
+                x: pos.x + this.cellSize * x,
+                y: pos.y + this.cellSize * y 
+            };
+            var Tile = this.tileMap[x][y].tile;
+            if (Tile != -1)
+                context.drawImage(this.tileDictionary[Tile], offset.x, offset.y, this.cellSize, this.cellSize);
+        }
+    }
+};
