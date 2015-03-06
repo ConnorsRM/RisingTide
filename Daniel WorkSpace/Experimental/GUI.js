3fec23b5877626e0ac4780e5cd06e2d9a1491bab
ConnorsRM <ryan@eduroam-237-175.ucsc.edu>
2015-03-06 14:15:03 -0800

    Prepare for GUI implementation
    It’s coming

---
+++ Daniel WorkSpace/Experimental/GUI.js
@@ -0,0 +1,26 @@
+/*
+ * GUI.js yadda yadda
+ */
+
+
+
+
+function GUI() {
+	
+	//Item fields
+	
+	//Store the index of the currently
+	//selected item
+	this.selected_item = 0;
+	
+	//store array of items the player
+	//player 
+	this.items = [];
+	
+	//store the hunger value of the 
+	//XX TODO
+	
+	//need sprites for the GUI
+	
+	
+}
