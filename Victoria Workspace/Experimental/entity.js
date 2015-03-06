991cfe4b88d7ed50897648869362b5fb1c07173d
vmercado <vmercado@ucsc.edu>
2015-03-06 02:25:21 -0800

    Story + Hunger
    The story is separated into journal entries, not sure how they would
    appear, either over time or with the score?
    And when I tried putting the hunger into player, the canvas didn’t
    display anything and it doesn’t display anything when the context.font
    is enabled

---
+++ Victoria Workspace/Experimental/entity.js
@@ -0,0 +1,22 @@
+function Entity(pos) {
+	this.x = pos.x;
+	this.y = pos.y;
+	this.spr = undefined;
+	this.speed = undefined;
+}
+
+Entity.prototype.draw = function(camera) {
+	if (this.spr == undefined) {
+		return;
+	}
+	
+	this.spr.draw(this.animationIndex, this.imageIndex,
+		this.x - this.spr.frameWidth / 2 - camera.x + camera.viewWidth, 
+		this.y - this.spr.frameWidth / 2 - camera.y + camera.viewHeight);	
+};
+
+Entity.prototype.update = function() {
+	//initially empty implementation,
+	//override in individual types
+};
+
