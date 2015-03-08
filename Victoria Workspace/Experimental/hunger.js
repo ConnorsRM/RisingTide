520afc25b885a26382e1907487402a158f403284
vmercado <vmercado@ucsc.edu>
2015-03-07 16:54:40 -0800

    Main Menu Screen
    Moved artwork to its own folder to keep it clean and made a main menu
    screen
    Not sure if thats the final background to use

--- Victoria Workspace/Experimental/hunger.js
+++ Victoria Workspace/Experimental/hunger.js
@@ -1,4 +1,6 @@
 var Hunger = function(x, y){
+	//allows the hunger bar to be placed anywhere
+	//given stats for the hunger is 100/100 at starting point
 	this.x = x;
 	this.y = y;
 	this.width = 100;
@@ -8,29 +10,43 @@ var Hunger = function(x, y){
 };
 
 Hunger.prototype.displayHunger = function(){
-	//bars for hunger
+	//function that will show the text and bars for hunger
+	
+	//The text of hunger
 	Context.fillStyle = "Black";
 	Context.font = "18px sans-serif");
 	Context.fillText("Hunger", 20, 20);
 	
+	//background of the hunger bar
 	Context.fillStyle = "Black";
 	Context.fillRect(this.x, this.y, this.width, this.height);
 	
+	//part of the hunger bar that will decrease/increase over time
 	Context.fillStyle = "Red";
 	Context.fillRect(this.x, this.y, this.width*percent, this.height);
 };
 
 Hunger.prototype.update = function(){
-	//checking hunger status
+	//checking hunger status to determine the bar length
+	
+	/*
+	 if(this.animationIndex = PlayerAnims.eatFood){
+		this.hunger += 10;
+	}
+	*/
+	
 	if(this.hungry <= 0){
+		//if player's health reaches 0, he dead
 		this.hungry = 0;
-		//end game status
+		this.Player.die();
 	}else if(this.hungry >= 101){
+		//stops player's health from going over the bar
 		this.hungry = 100;
-		this.displayHungry();
+		this.displayHunger();
 	}else{
+		//slowly depletes the health bar
 		this.hungry -= 0.05;
 		this.percent = this.hungry/this.maxHungry;
-		this.displayHungry();
+		this.displayHunger();
 	}
 };
