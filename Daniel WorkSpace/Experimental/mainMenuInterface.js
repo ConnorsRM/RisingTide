915ea604aca9049251d185bef8b2e7576c752749
Daniel Luna <danieljluna0@gmail.com>
2015-03-13 00:41:55 -0700

    Main Menu Finished
    Now that I figured out just what needs to be done to create a new
    interface, and I've thoroughly tested it, I might just make a high score
    screen tomorrow morning....should be easy enough

--- Daniel WorkSpace/Experimental/mainMenuInterface.js
+++ Daniel WorkSpace/Experimental/mainMenuInterface.js
@@ -1,7 +1,50 @@
 //Main Menu set up for Game
 
 var mainMenu = new Interface();
+var MenuBackground = new Image();
 
 mainMenu.init = function() {
+    //Initializing Internal Vars
     this.id = InterfaceStack.push(this);
+    this.active = true;
+    this.visible = true;
+    this.drawFrame = 0;
+    this.updateFrame = 0;
+    
+    
+    this.obj_array.push(new Menu());
+    //Input Definitions
+    this.inputHandler = function(e, status) {
+        if ((e.keyCode == 13) && !status && this.active) {  //Enter
+            mainGame.init();
+            this.visible = false;
+        } else if ((e.keyCode == 84) && !status) { //T
+            this.active = !this.active;
+        } else if (!this.active) {
+             thist.active = true;
+        }
+    };
+};
+
+
+var Menu = function() {};
+
+
+Menu.prototype.update = function() {
+    
+};
+
+
+Menu.prototype.draw = function() {
+    //Draw Background
+    Context.drawImage(MenuBackground, 0, 0, 800, 600);
+    
+    //The text Buttons
+    Context.textAlign = "center";
+    Context.fillStyle = "Green";
+    Context.font = "32px sans-serif";
+    Context.fillText("Play Game: Enter", 400, 300);
+    Context.fillText("Read Tutorial: T", 400, 350);
+    
+    Context.textAlign = "start";
 };
